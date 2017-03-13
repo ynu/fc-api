363ac6975eb2c3e1b16a1a7e1c0d09c666b074fc
liudonghua123 <liudonghua123@gmail.com>
2017-03-13 14:06:49 +0800

    modify huawei esdk code

--- src/main/java/com/liudonghua/api/fc/rest/AuthController.java
+++ src/main/java/com/liudonghua/api/fc/rest/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.huawei.esdk.fusioncompute.local.ServiceFactory;
 import com.huawei.esdk.fusioncompute.local.model.ClientProviderBean;
 import com.huawei.esdk.fusioncompute.local.model.FCSDKResponse;
+import com.huawei.esdk.fusioncompute.local.model.SDKCommonResp;
 import com.huawei.esdk.fusioncompute.local.model.common.LoginResp;
 import com.huawei.esdk.fusioncompute.local.resources.common.AuthenticateResource;
 import com.liudonghua.api.fc.util.Utils;
@@ -35,14 +36,32 @@ public class AuthController {
 	 * </pre>
 	 * </code>
 	 */
-	@RequestMapping("/")
+	@RequestMapping("/login")
 	public FCSDKResponse login(HttpSession session) {
+		String userName = "api";
 		ClientProviderBean clientProvider = Utils.initClientProviderBean();
+		clientProvider.setUserName(userName);
 		// 初始化用户资源实例
 		AuthenticateResource auth = ServiceFactory.getService(AuthenticateResource.class, clientProvider);
 		// 以用户名，用户密码作为传入参数，调用AuthenticateResource提供的login方法，完成用户的登录
-		FCSDKResponse<LoginResp> loginResponse = auth.login("test", "zaq1XSW@");
+		FCSDKResponse<LoginResp> loginResponse = auth.login(userName, "zaq1XSW@");
 		session.setAttribute("clientProvider" , clientProvider);
 		return loginResponse;
+	}
+
+	@RequestMapping("/logout")
+	public SDKCommonResp logout(HttpSession session) {
+		ClientProviderBean clientProvider = (ClientProviderBean) session.getAttribute("clientProvider");
+		if(clientProvider != null) {
+			String userName = "api";
+			clientProvider.setUserName(userName);
+			// 初始化用户资源实例
+			AuthenticateResource auth = ServiceFactory.getService(AuthenticateResource.class, clientProvider);
+			// 以用户名，用户密码作为传入参数，调用AuthenticateResource提供的login方法，完成用户的登录
+			SDKCommonResp logoutResponse = auth.logout();
+			session.setAttribute("clientProvider" , null);
+			return logoutResponse;
+		}
+		return new SDKCommonResp();
 	}
 }
